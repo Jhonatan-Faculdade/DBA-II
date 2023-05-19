de1c96c4f495941649b2301955f1551e8b56303c
jhonatan <rizzijhonatan17@gmail.com>
2023-05-18 21:54:53 -0300

    atualizado modelss

--- models/_all_models.py
+++ models/_all_models.py
@@ -1,10 +1,5 @@
 from models.usuario_model import UsuarioModel
 from models.categoria_model import CategoriaModel
 from models.produto_model import ProdutoModel
-<<<<<<< HEAD
 from models.produto_foto_model import ProdutoFotos
 from models.restaurante_model import RestauranteModel
-=======
-from models.produto_fotos_model import ProdutoFotos
-from models.restaurante_model import RestauranteModel
->>>>>>> ccc132a80a70250c0b6e32c20b03c863ff38c16a
--- models/categoria_model.py
+++ models/categoria_model.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import relationship
 
 
 class CategoriaModel(settings.DBBaseModel):
-<<<<<<< HEAD
     __tablename__ = 'categorias'
 
     id: int = Column(Integer, primary_key=True, autoincrement=True)
@@ -19,16 +18,3 @@ class CategoriaModel(settings.DBBaseModel):
         uselist=True,
         lazy="joined"
     )
-=======
-    __tablename__ = 'categoria'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    descricao = Column(String(100), nullable=False)
-    icone = Column(String(300), nullable=True)
-    ativa = Column(Boolean, default=True)
-    produtos = relationship("CategoriaModel", 
-                            cascade="all, delete-orphan", 
-                            back_populates='categoria', 
-                            uselist=True, 
-                            lazy="joined")
->>>>>>> ccc132a80a70250c0b6e32c20b03c863ff38c16a
